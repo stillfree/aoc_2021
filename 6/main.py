345c732323e7c389d2c7ba5c4112b513a214476c
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2021-12-06 06:58:58 +0100

    day6

--- 6/main.py
+++ 6/main.py
@@ -10,14 +10,41 @@ def readInput():
     return lines
 
 def partOne( lines ):
-    pass
+    area = []
+    newArea = []
+    for i in range(9):
+        area.append(0)
+        newArea.append(0)
+        area[i] = lines[0].count(str(i))
+    day = 0
+    while day != 256:
+        newArea[0] = area[1]
+        newArea[1] = area[2]
+        newArea[2] = area[3]
+        newArea[3] = area[4]
+        newArea[4] = area[5]
+        newArea[5] = area[6]
+        newArea[6] = area[0] + area[7]
+        newArea[7] = area[8]
+        newArea[8] = area[0]
+        area = list(newArea)
+        day += 1
+        summe = 0
+        for i in range(0,9):
+            summe += area[i]
+        if(day == 80):
+            print("PartOne:", summe)
+    summe = 0
+    for i in range(0,9):
+        summe += area[i]
+    print("PartTwo:",summe)
 
 def partTwo( lines ):
     pass
 
 def main():
     lines = readInput()
-    print(lines)
+    #print(lines)
     partOne( lines )
     partTwo( lines )
 
