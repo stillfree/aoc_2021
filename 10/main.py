5d29f02138f8e84772fee3d48f80ff5c2df11d0d
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2021-12-10 07:37:47 +0100

    Day 10

--- 10/main.py
+++ 10/main.py
@@ -10,14 +10,43 @@ def readInput():
     return lines
 
 def partOne( lines ):
-    pass
+    summer = { ")":3, "]":57, "}":1197, ">":25137 }
+    summe2 = { ")":1, "]":2, "}":3, ">":4 }
+    compare = { ")":"(", "]":"[", "}":"{", ">":"<" }
+    compare2 = { "(":")", "[":"]", "{":"}", "<":">" }
+    result = []
+
+    valid = []
+    counter = 0
+    for line in lines:
+        stack = []
+        val = True
+        for char in line:
+            if char in ["(", "{", "[", "<"]:
+                stack.append(char)
+            else:
+                curr = stack.pop()
+                if( curr != compare[char] ):
+                    counter += summer[char]
+                    val = False
+                    break
+        if val:
+            print(stack)
+            counter2 = 0
+            for i in range(len(stack)):
+                counter2 *= 5
+                counter2 += summe2[compare2[stack.pop()]]
+            result.append(counter2)
+
+    print(counter)
+    result.sort()
+    print(result[int(len(result)/2)])
 
 def partTwo( lines ):
     pass
 
 def main():
     lines = readInput()
-    print(lines)
     partOne( lines )
     partTwo( lines )
 
